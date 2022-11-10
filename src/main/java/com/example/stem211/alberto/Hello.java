33899b869605b25e77ee4c5e0bdaaacd76edd56b
AlToscani <93136804+AlToscani@users.noreply.github.com>
2022-11-10 15:13:59 +0100

    added a '!' in line 5

--- src/main/java/com/example/stem211/alberto/Hello.java
+++ src/main/java/com/example/stem211/alberto/Hello.java
@@ -2,7 +2,7 @@ package com.example.stem211.alberto;
 
 public class Hello {
 		public static void main(String[] args) {
-			System.out.println("Nel mentre saluto anch'io");
+			System.out.println("Nel mentre saluto anch'io!");
 			System.out.println("Hello world!");
 		}
 }
