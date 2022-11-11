4689597e52110553ce9ba4034dbaa6a17a7799e6
Emanuele <egalli64@gmail.com>
2022-11-11 08:20:04 +0100

    checkstyle

--- src/main/java/com/example/stem211/emanueleGalli/Hello.java
+++ src/main/java/com/example/stem211/emanueleGalli/Hello.java
@@ -1,7 +1,10 @@
 package com.example.stem211.emanueleGalli;
 
+import com.example.stem211.emanuele.*;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("See you, Emanuele!");
+        System.out.println(HelloManuel.class);
     }
 }
