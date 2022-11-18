87fe51d02329da319d6449a66e6461b529377a75
Emanuele <egalli64@gmail.com>
2022-11-18 14:54:24 +0100

    test support

--- src/main/java/com/example/stem211/emanueleGalli/bingo/Game.java
+++ src/main/java/com/example/stem211/emanueleGalli/bingo/Game.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Game {
+    public int f() {
+        return 0;
+    }
+
     public static void main(String[] args) {
         System.out.println("Starting the game!");
 
---
+++ src/test/java/com/example/stem211/emanueleGalli/bingo/GameTest.java
@@ -0,0 +1,12 @@
+package com.example.stem211.emanueleGalli.bingo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+    @Test
+    void testF() {
+        fail("Not yet implemented");
+    }
+}
